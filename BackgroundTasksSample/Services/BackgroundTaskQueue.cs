4ba7c4eec8bea5566619002becf019999cc717f1
hatsuyama <hatsuyama>
2025-03-03 19:30:04 +0900

    add

--- BackgroundTasksSample/Services/BackgroundTaskQueue.cs
+++ BackgroundTasksSample/Services/BackgroundTaskQueue.cs
@@ -1,11 +1,11 @@
 using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace BackgroundTasksSample.Services;
 
-#region snippet1
 public interface IBackgroundTaskQueue
 {
     ValueTask QueueBackgroundWorkItemAsync(Func<CancellationToken, ValueTask> workItem);
@@ -57,4 +57,51 @@ public class BackgroundTaskQueue : IBackgroundTaskQueue
         return Task.CompletedTask;
     }
 }
-#endregion
+
+public class BackgroundTaskQueueSimple : IBackgroundTaskQueue
+{
+    private readonly BlockingCollection<Func<CancellationToken, ValueTask>> _queue;
+
+    public BackgroundTaskQueueSimple(int capacity)
+    {
+        // キューの最大容量を設定
+        _queue = new BlockingCollection<Func<CancellationToken, ValueTask>>(
+            new ConcurrentQueue<Func<CancellationToken, ValueTask>>(), capacity);
+    }
+
+    public ValueTask QueueBackgroundWorkItemAsync(
+        Func<CancellationToken, ValueTask> workItem)
+    {
+        if (workItem == null) throw new ArgumentNullException(nameof(workItem));
+
+        try
+        {
+            _queue.Add(workItem); // キューにタスクを追加
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine("Queue is closed for adding new tasks.");
+        }
+
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask<Func<CancellationToken, ValueTask>> DequeueAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var workItem = _queue.Take(cancellationToken); // キューからタスクを取得（空なら待機）
+            return new ValueTask<Func<CancellationToken, ValueTask>>(workItem);
+        }
+        catch (OperationCanceledException)
+        {
+            //return new ValueTask<Func<CancellationToken, ValueTask>>(() => new ValueTask());
+        }
+    }
+
+    public void Dispose()
+    {
+        _queue.CompleteAdding(); // キューの追加を終了
+        _queue.Dispose();
+    }
+}
